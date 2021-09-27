1a7d183ae251aa0b68a931f845c16cc46b1922aa
Nic Deane <njdeane7@gmail.com>
2021-09-27 16:05:26 +1000

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Nic Deane on 27/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
